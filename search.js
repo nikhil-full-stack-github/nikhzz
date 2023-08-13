a8d442233414cc5af0c3828bdbbbdbcba2c293f7
Nikhil P <134950904+nikhil-full-stack-github@users.noreply.github.com>
2023-08-13 21:36:54 +0530

    search

---
+++ search.js
@@ -0,0 +1,9 @@
+let dogs=["German Shepherd","Bulldog","Labrador Retriever","Golden Retriever","French Bulldog","Siberian Husky"];
+           
+       for(let i=0; i<6; i++){
+        if(dogs[i]==="Golden Retriever"){
+           console.log("found golden retriever")
+           }
+        }
+       
+       
